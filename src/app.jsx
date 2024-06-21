b9376bd0a9bbdcccf94182ebbc3f1bebfdf05d71
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-21 14:40:17 +0200

    fetching the data

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css"
 import images from './images/error-message.png'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+          throw new Error('Data fetcing Failed');
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
